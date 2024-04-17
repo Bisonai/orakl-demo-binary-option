28fc683b1196857699d6f236956fd9eebcdfef0e
Martin Kersner <martin@bisonai.com>
2024-04-17 22:12:42 +0900

    chore: keep single ABI file

--- src/contracts/UpDownContract.ts
+++ src/contracts/UpDownContract.ts
@@ -1,17 +1,19 @@
 import { ethers } from "ethers";
 import { ILatestRound } from "../types";
 import { BaseInterface } from "./interfaces";
-import { getAbiByAddress } from "./utils/getAbis";
+import AGGREGATOR_ABI from "./abis/AggregatorABI.json";
 
 export class UpDownContract extends BaseInterface {
   constructor(provider: ethers.providers.Web3Provider, smAddress: string) {
-    super(provider, smAddress, getAbiByAddress(smAddress));
+    super(provider, smAddress, AGGREGATOR_ABI);
   }
 
   latestRoundDataAsync = async (): Promise<ILatestRound> => {
     const rs = await this._contract.latestRoundData();
+    const decimals = await this._contract.decimals();
+
     return {
-      answer: this._toNumber(rs.answer) / Math.pow(10, 8),
+      answer: this._toNumber(rs.answer) / Math.pow(10, decimals),
       answeredInRound: this._toNumber(rs.answer),
       roundId: this._toNumber(rs.roundId),
       startedAt: this._toNumber(rs.startedAt),
